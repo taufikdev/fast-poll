cfe7c0d81cbd44651df62e383d6d282e985d943f
elazzaoui55 <mhamedelazzaouui17@gmail.com>
2022-02-28 21:14:22 +0100

    creation des migrations

---
+++ database/migrations/2022_02_28_181753_create_profils_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profils', function (Blueprint $table) {
+            $table->bigIncrements('Id_Profil');
+            $table->unsignedBigInteger('Id_Acc');
+            $table->foreign('Id_Acc')->references('Id_Acc')->on('accounts');
+            $table->unsignedBigInteger('Id_Add');
+            $table->foreign('Id_Add')->references('Id_Add')->on('address');
+            $table->string('First_Name');
+            $table->string('Last_Name');
+            $table->string('Description');
+            $table->string('Experience');
+            $table->binary('photo');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profils');
+    }
+};
